9e24d7125e5d22b39e82f9cc56c5c40f935de77b
Rosy Tucker <rosy533@gmail.com>
2014-07-30 17:58:53 +0100

    Make Basic Form Input

--- SpringTest/src/main/java/uk/co/iceroad/HelloController.java
+++ SpringTest/src/main/java/uk/co/iceroad/HelloController.java
@@ -2,6 +2,7 @@ package uk.co.iceroad;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -11,6 +12,14 @@ public class HelloController {
 	@RequestMapping(method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
 		model.addAttribute("message", "Hello world!");
-		return "hello";
+        model.addAttribute("user", new User());
+        return "hello";
 	}
+
+    @RequestMapping(method = RequestMethod.POST)
+    public String submitForm(@ModelAttribute User user, ModelMap model) {
+        model.addAttribute("user", user);
+        model.addAttribute("message", "Successfully saved person: " + user.getFirstName() + "  " + user.getSurname());
+        return "hello";
+    }
 }
